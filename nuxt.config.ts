4dd28cdd393d8dab5f5103999da55ba471047f1b
Olga Brendak <olga.brendak.dg@gmail.com>
2025-07-31 17:23:47 +0300

    fix alias

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   alias: {
-    "@": "./",
+    "@": resolve(__dirname, "./"),
   },
   modules: ["@pinia/nuxt"],
   app: {
