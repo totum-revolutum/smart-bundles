d7c98ca6c41868e019edabfdd1bd1fad32b8c20b
Olga Brendak <olga.brendak.dg@gmail.com>
2025-07-31 17:59:26 +0300

    Deploy func to GitHub Pages

---
+++ _nuxt/CAP3WcKY.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as c}from"./CmoBUVN-.js";const n={};function o(r,s){return c(),t("div",null,"Active Investment Page (в розробці)")}const _=e(n,[["render",o]]);export{_ as default};
---
+++ _nuxt/Tosf7a0l.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as c}from"./CmoBUVN-.js";const r={};function t(n,s){return c(),o("div",null,"How it work Page (в розробці)")}const _=e(r,[["render",t]]);export{_ as default};
